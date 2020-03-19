18d32c8dec2b8c9b566b74050654aa0a439f2441
SpyrosGeo <xarodasimba@gmail.com>
2020-03-19 20:47:05 +0200

    completed RESTful api for postgres by implementing  delete todo functionality

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.post("/todos", async (req, res) => {
     try {
         const { description } = req.body
         const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
-         [description])
+            [description])
         res.json(newTodo.rows[0]);
     } catch (err) {
         console.error(err.message)
@@ -25,7 +25,7 @@ app.post("/todos", async (req, res) => {
 })
 
 //get all todos
-app.get("/todos", async(req,res)=>{
+app.get("/todos", async (req, res) => {
     try {
         const allTodos = await pool.query("SELECT * FROM todo")
         res.json(allTodos)
@@ -34,20 +34,40 @@ app.get("/todos", async(req,res)=>{
     }
 })
 //get a todo
-app.get('/todos/:id',async(req,res)=>{
+app.get('/todos/:id', async (req, res) => {
     try {
-        const {id} = req.params
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1",[id]) 
-        res.json(todo.rows[0])       
+        const { id } = req.params
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+        res.json(todo.rows[0])
     } catch (err) {
         console.error(err.message)
     }
 })
 
 //edit a todo
-
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1 WHERE todo_id = $2",
+        [description,id])
+    
+        res.json("todo was updated")
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 //delete a todo
-
+app.delete("/todos/:id", async(req,res)=>{
+    try {
+        const {id} =req.params
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1",[id])
+        res.json("Todo was deleted")
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`server is up at port ${PORT}`)
