16796742daf7939c3718b373197d841ea81b16a9
SpyrosGeo <xarodasimba@gmail.com>
2020-03-19 20:18:06 +0200

    implemented get all todos functionality and get a single todo

--- server/index.js
+++ server/index.js
@@ -17,16 +17,32 @@ app.post("/todos", async (req, res) => {
         const { description } = req.body
         const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
          [description])
-        res.json(newTodo)
+        res.json(newTodo.rows[0]);
     } catch (err) {
-        console.error(err)
+        console.error(err.message)
     }
 
 })
 
 //get all todos
-
+app.get("/todos", async(req,res)=>{
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo")
+        res.json(allTodos)
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 //get a todo
+app.get('/todos/:id',async(req,res)=>{
+    try {
+        const {id} = req.params
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1",[id]) 
+        res.json(todo.rows[0])       
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 
 //edit a todo
 
